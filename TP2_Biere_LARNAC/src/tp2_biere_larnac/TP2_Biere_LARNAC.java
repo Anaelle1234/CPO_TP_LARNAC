912947d4ddcbfc888f352f73c710138d2a7ed2c0
anaelle larnac <anaelle.larnac@epfedu.fr>
2022-10-25 09:58:18 +0200

    fin de la première partie + ajout des autres bières et affichage si la bière est décapsulée ou non

--- TP2_Biere_LARNAC/src/tp2_biere_larnac/TP2_Biere_LARNAC.java
+++ TP2_Biere_LARNAC/src/tp2_biere_larnac/TP2_Biere_LARNAC.java
@@ -18,12 +18,18 @@ public class TP2_Biere_LARNAC {
     
     BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls",7.0,"Dubuisson") ;
     BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6,"Abbaye de Leffe");  
-    BouteilleBiere secondeBiere = new BouteilleBiere("Guiness",5.0,"Dublin"); 
+    BouteilleBiere secondeBiere = new BouteilleBiere("Guiness",5.0,"Dublin");
+    BouteilleBiere troisiemeBiere = new BouteilleBiere("HK",5.2,"Hollande");
+    BouteilleBiere quatriemeBiere = new BouteilleBiere("1664",5.4,"Europe");
+    
+    
     
     uneBiere.Décapsuler();
     System.out.println(uneBiere);
     System.out.println(autreBiere);
     System.out.println(secondeBiere);
+    System.out.println(troisiemeBiere);
+    System.out.println(quatriemeBiere);
     
  
 }
