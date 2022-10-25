a67a37efad6bbb1c7005ffc2e586b58758dcc392
anaelle larnac <anaelle.larnac@epfedu.fr>
2022-10-25 12:36:09 +0200

    association des voitures de bob et de reno

--- TP2_relation1_LARNAC/src/tp2_relation1_larnac/TP2_relation1_LARNAC.java
+++ TP2_relation1_LARNAC/src/tp2_relation1_larnac/TP2_relation1_LARNAC.java
@@ -25,8 +25,18 @@ public class TP2_relation1_LARNAC {
         bob.liste_voitures[0] = uneClio ;
         bob.nbVoitures = 1 ;
         uneClio.Proprietaire = bob ;
-        System.out.println("la premiere voiture de Bob est " + bob.liste_voitures[0] ) ;
-
+        System.out.println("la premiere voiture de Bob est " + bob.liste_voitures[0]) ;
+        bob.liste_voitures[1]= uneAutreClio;
+        bob.nbVoitures=2;
+        System.out.println("la deuxième voiture de Bob est " + bob.liste_voitures[1]) ;
+        reno.liste_voitures[0] = une2008 ;
+        reno.nbVoitures = 1 ;
+        une2008.Proprietaire = reno ;
+        System.out.println("la premiere voiture de Reno est " + reno.liste_voitures[0]) ;
+        reno.liste_voitures[1] = uneMicra ;
+        reno.nbVoitures = 2 ;
+        uneMicra.Proprietaire = reno ;
+        System.out.println("la deuxième voiture de Reno est " + reno.liste_voitures[1]) ;
     }
     
     
