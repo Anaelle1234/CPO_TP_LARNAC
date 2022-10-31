be8ede297023aa2fad6c1388fc2a585f3caf0555
anaelle larnac <anaelle.larnac@epfedu.fr>
2022-10-31 14:38:18 +0100

    Tp1 manip nombres int fini

--- TP1_manipNombresInt_LARNAC/src/tp1_manipnombresint_larnac/TP1_manipNombresInt_LARNAC.java
+++ TP1_manipNombresInt_LARNAC/src/tp1_manipnombresint_larnac/TP1_manipNombresInt_LARNAC.java
@@ -4,6 +4,8 @@
  */
 package tp1_manipnombresint_larnac;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Anaëlle
@@ -14,7 +16,32 @@ public class TP1_manipNombresInt_LARNAC {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int nb1;
+        double nb2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Entrer un premier nombre");
+        nb1=sc.nextInt();
+        System.out.println("\n Entrer un deuxième nombre");
+        nb2=sc.nextInt();
+        System.out.println("\n Le premier nombre est : "+nb1);
+        System.out.println("\n Le deuxième nombre est : "+nb2);
+        double Add;
+        Add=nb1+nb2;
+        System.out.println("addition des 2 nombres choisis donne : "+nb1+" + "+nb2+" = "+Add);
+        
+        double Diff;
+        Diff=nb1-nb2;
+        System.out.println("soustraction des 2 nombres choisis donne : "+nb1+" - "+nb2+" = "+Diff);
+        
+        double Mult;
+        Mult=nb1*nb2;
+        System.out.println("multiplication des 2 nombres choisis donne : "+nb1+" * "+nb2+" = "+Mult);
+        
+        double Div;
+        Div=nb1/nb2;
+        double Mod;
+        Mod=nb1%nb2;
+        System.out.println("division des 2 nombres choisis donne : "+nb1+" / "+nb2+" = "+Div+"\n"+" le reste de la divison est de : "+Mod);
     }
     
 }
